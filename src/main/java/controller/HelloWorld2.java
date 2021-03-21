c75db8c872e0cb12e7214b8a949fd793e7e14d13
ujn_sunqiang <ujn_sunqiang@163.com>
2021-03-21 19:09:25 +0800

    项目第一次push到git

--- src/main/java/controller/HelloWorld2.java
+++ src/main/java/controller/HelloWorld2.java
@@ -2,7 +2,7 @@ package controller;
 
 public class HelloWorld2 {
     public static void main(String[] args) {
-        System.err.println("dev修改");
+
         System.err.println("dev修改2");
 
     }
