3dd49c4a047dfe03013ea6ddaac4294c787364d1
ujn_sunqiang <ujn_sunqiang@163.com>
2021-04-06 22:47:06 +0800

    。。。

--- src/main/java/controller/HelloWorld2.java
+++ src/main/java/controller/HelloWorld2.java
@@ -4,8 +4,6 @@ public class HelloWorld2 {
     public static void main(String[] args) {
 
         System.err.println("...");
-        System.err.println("dev修改22222");
-        System.err.println("0406...");
         System.err.println("0406...111222");
 
         System.err.println("...");
