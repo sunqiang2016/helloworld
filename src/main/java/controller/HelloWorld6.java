bd7613b34cce61a8e9c6da2655c600f20098c258
ujn_sunqiang <ujn_sunqiang@163.com>
2021-04-18 18:07:24 +0800

    dev commit 2

--- src/main/java/controller/HelloWorld6.java
+++ src/main/java/controller/HelloWorld6.java
@@ -5,5 +5,7 @@ public class HelloWorld6 {
     public static void main(String[] args) {
         System.err.println("1");
         System.err.println("2");
+        System.err.println("3");
+
     }
 }
